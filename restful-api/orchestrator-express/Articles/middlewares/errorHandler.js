eb305a31e01983d761c7accacd118ab97a28c67e
Amilio A <amilio@mcpayment.co.id>
2022-11-05 22:16:20 +0700

    remove unused code

--- restful-api/orchestrator-express/Articles/middlewares/errorHandler.js
+++ restful-api/orchestrator-express/Articles/middlewares/errorHandler.js
@@ -1,7 +1,5 @@
 module.exports = (err, req, res, next) => {
   console.log(err, "<<<<<<<:ERROR MASUK DARI ERROR HANDLER");
-  console.log(err.name, "<<<<<<:ERROR NAME");
-  console.log(err.message, "<<<<<<<:ERROR MSG");
   let statusCode = 500;
   let errorCode = "Internal server error";
   let message = "Unexpected error.";
--- restful-api/orchestrator-express/Comments/middlewares/errorHandler.js
+++ restful-api/orchestrator-express/Comments/middlewares/errorHandler.js
@@ -1,39 +1,19 @@
 module.exports = (err, req, res, next) => {
-  console.log(err, '<<<<<<<:ERROR MASUK DARI ERROR HANDLER')
-  console.log(err.name, '<<<<<<:ERROR NAME')
-  console.log(err.message, '<<<<<<<:ERROR MSG')
-  let statusCode = 500
-  let errorCode = 'Internal server error'
-  let message = 'Unexpected error.'
+  console.log(err, "<<<<<<<:ERROR MASUK DARI ERROR HANDLER");
+  let statusCode = 500;
+  let errorCode = "Internal server error";
+  let message = "Unexpected error.";
 
   switch (err.name) {
-    case 'error_400_username_email_password_empty':
-      statusCode = 400
-      errorCode = 'Validation error'
-      message = 'Please enter username, email and password'
-      break
-      
-    case 'error_400_email_password_empty':
-      statusCode = 400
-      errorCode = 'Validation error'
-      message = 'Please enter email and password'
-      break
-
-    case 'error_400_wrong_email_password':
-      statusCode = 400
-      errorCode = 'Validation error'
-      message = 'Wrong email or password'
-      break
-
-    case 'error_404_user_not_found':
-      statusCode = 404
-      errorCode = 'Not Found'
-      message = 'Requested user was not found'
-      break
+    case "error_500":
+      statusCode = 500;
+      errorCode = "Internal Server Error";
+      message = "Internal Server Error";
+      break;
 
     default:
-      break
+      break;
   }
 
-  res.status(statusCode).json({ errorCode, message })
-}
+  res.status(statusCode).json({ errorCode, message });
+};
